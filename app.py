d72daa0b47cd665eb9c38da347319e43393136a0
gmeeder <guillermo.meeder@gmail.com>
2023-11-16 09:31:00 -0300

    v8
    v8

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 from flask import Flask, render_template
 import sqlite3
+import os
 
 app = Flask(__name__)
 
-# Ruta para la página principal
-@app.route('/')
-def index():
+# Ruta para generar las páginas estáticas
+@app.route('/generate_static_pages')
+def generate_static_pages():
     try:
         # Conéctate a la base de datos SQLite
         connection = sqlite3.connect('datosterrenodb.sqlite')
@@ -20,11 +21,26 @@ def index():
         # Cierra la conexión
         connection.close()
 
-        # Renderiza la plantilla HTML y pasa los datos a la página
-        return render_template('index.html', rows=rows)
+        # Genera las páginas estáticas en el directorio 'docs'
+        generate_static_html(rows)
+
+        return "Páginas estáticas generadas con éxito."
 
     except sqlite3.Error as err:
         return f"Error: {err}"
+
+# Función para generar las páginas estáticas
+def generate_static_html(rows):
+    if not os.path.exists('docs'):
+        os.makedirs('docs')
+
+    # Renderiza la plantilla HTML y guarda cada página como un archivo HTML estático
+    for row in rows:
+        html_content = render_template('static_page_template.html', row=row)
+
+        filename = f"docs/{row[0]}_{row[1]}.html"  # Puedes ajustar el nombre del archivo según tus necesidades
+        with open(filename, 'w', encoding='utf-8') as file:
+            file.write(html_content)
 
 if __name__ == '__main__':
     app.run(debug=True)
